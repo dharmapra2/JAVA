cefaa9c977318cc4686c24dd6ed5ef80eb8b958f
dharmapra2 <dharmapra865@gmail.com>
2021-01-27 22:05:24 +0530

    commit

---
+++ 30DAYS_CODE_gfg/gfg2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+class gfg2{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            long n=sc.nextLong();
+            Solution s=new Solution();
+            long nth=s.findNth(n);
+            System.out.println(nth);
+        }
+    }
+}
+class Solution {
+ long findNth(long N)
+    {
+       long ans=0;
+long mul=1;
+while(N!=0)
+{
+long dig=N%9;
+N=N/9;
+ans+=(mul*dig);
+mul=mul*10;
+}
+return ans;
+        }
+    }
---
+++ Basics/gfg3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class gfg3 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            System.out.println(new GfG().convertfive(N));
+            T--;
+        }
+    }
+}// } Driver Code Ends
+
+
+class GfG {
+    int convertfive(int n) {
+        // Your code here
+        int num=0,count=1,rem,res;
+        boolean b = (n!=0);
+        while(b){
+            res=n/10;
+            rem=n%10;
+            if(rem==0){
+                num=num+count*5;
+            }
+            else{
+                num=num+rem*count;
+            }
+            count=count*10;
+            n=n/10;
+            b = (n!=0);
+        }
+        return num;
+    }
+}
