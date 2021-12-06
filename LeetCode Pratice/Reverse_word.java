c68aef4cad79d13dac7578b095cc4cf9be24b5ff
dharmapra2 <dharmapra865@gmail.com>
2021-12-06 20:02:11 +0530

    commit

---
+++ LeetCode Pratice/Reverse_word.java
@@ -0,0 +1,12 @@
+public class Reverse_word {
+    public String reverseWords(String s) {
+        String str[]=s.split("\\s+");
+        String str1="";
+        for(String s1:str){
+            StringBuilder sb=new StringBuilder(s1);  
+            sb.reverse();  
+            str1+=sb.toString()+" ";  
+        }
+        return str1.trim();
+    }
+}
