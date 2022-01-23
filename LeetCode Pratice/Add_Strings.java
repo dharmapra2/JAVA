719bfdca7719784de7866078077f3ea6ac934317
dharmapra2 <dharmapra865@gmail.com>
2022-01-23 15:30:18 +0530

    add

---
+++ LeetCode Pratice/Add_Strings.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+        int carry = 0;
+        int p1 = num1.length() - 1;
+        int p2 = num2.length() - 1;
+        while (p1 >= 0 || p2 >= 0) {
+            int x1 = p1 >= 0 ? num1.charAt(p1) - '0' : 0;
+            int x2 = p2 >= 0 ? num2.charAt(p2) - '0' : 0;
+            int value = (x1 + x2 + carry) % 10;
+            carry = (x1 + x2 + carry) / 10;
+            res.append(value);
+            p1--;
+            p2--;    
+        }
+        
+        if (carry != 0)
+            res.append(carry);
+        
+        return res.reverse().toString();
+    }
+}
+public class Add_Strings {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str1=sc.next();
+        String str2=sc.next();
+        Solution s=new Solution();
+        String sum=s.addStrings(str1, str2);
+        System.out.println(sum);
+        sc.close();
+    }
+}
