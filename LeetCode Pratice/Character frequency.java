2fc46cb8902b43ee08ffcb693eec0b97f354cac9
dharmapra2 <dharmapra865@gmail.com>
2022-01-24 15:20:55 +0530

    add

---
+++ LeetCode Pratice/Character frequency.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.regex.*;
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        int q=sc.nextInt();
+        while(q-->0){
+            int pos=sc.nextInt();
+            int count=0;
+            String s1=str.substring(0,pos-1);
+            String s=Character.toString(str.charAt(pos-1));
+            Pattern p=Pattern.compile(s);
+            Matcher m=p.matcher(s1);
+            while(m.find()){
+                count++;
+            }
+            System.out.println(count);
+        }
+	}
+}
---
+++ LeetCode Pratice/sequentialDigits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        String digits="123456789";
+        List<Integer> num = new ArrayList<Integer>();
+        int n1=String.valueOf(low).length();
+        int n2=String.valueOf(high).length();
+        for(int i=n1;i<=n2;i++){
+            for(int j=0;j<10-i;j++){
+                int digit=Integer.parseInt(digits.substring(j,j+i));
+                if(digit>=low && digit<=high)
+                    num.add(digit);
+            }
+        }
+        return num;
+    }
+}
