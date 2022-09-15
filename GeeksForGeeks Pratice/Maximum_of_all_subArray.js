f22cd8b17062dbf892fe4eb6d6bc462ba36fb22e
Dharma Pradhan <dharma.pradhan@nettantra.net>
2022-09-16 00:08:10 +0530

    commit

--- GeeksForGeeks Pratice/Maximum_of_all_subArray.js
+++ GeeksForGeeks Pratice/Maximum_of_all_subArray.js
@@ -8,16 +8,20 @@ function max_of_subarrays(arr, n, k) {
   if (n == 1) {
     return arr;
   }
-  let maxShift = n - k;
   let maxArr = [];
-  for (let i = 0; i <= maxShift; i++) {
-    let right = i + k;
-    let maxNo = Math.max(...arr.slice(i, right));
-    maxArr.push(maxNo);
+  let max = arr[0];
+  for (let i = 1, j = 1; i < n; i++, j++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (j >= k - 1) {
+      maxArr.push(max);
+    }
   }
+  if (maxArr.length == 0) maxArr.push(max);
   return maxArr;
 }
-let arr = [1,6,3,6,345,67,34,6,367,36,345,456,456,567,235,5756,2345,234,4,2,5,35,67,3,6,3,43,1,6,3,6,345,67,34,6,367,36,345,456,456,567,235,5756,2345,234,4,2,5,35,67,3,6,3,43];
+let arr = [10];
 let k = 5;
 // console.log(arr.slice(0, 3), arr);
 console.log(max_of_subarrays(arr, arr.length, k));
