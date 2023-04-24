1a680210a7daa42f9ab2281798b35610e89d17de
Dharmapra2 <dharmapra865@gmail.com>
2023-04-24 17:16:16 +0530

    findout longest common subsequence using recursion and memozie

--- LeetCode Pratice/longest_comm_subseq_1143.java
+++ LeetCode Pratice/longest_comm_subseq_1143.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 class longest_comm_subseq_1143 {
 
   public static void main(String[] args) {
-    String text1 = "abcde", text2 = "ace";
+    String text1 = "abc", text2 = "abc";
     Solution sol = new Solution();
     System.out.println(sol.longestCommonSubsequence(text1, text2));
   }
@@ -9,8 +11,41 @@ class longest_comm_subseq_1143 {
 
 class Solution {
 
+  private int[][] memo;
+
   public int longestCommonSubsequence(String text1, String text2) {
-    
-    return 0;
+    int m = text1.length();
+    int n = text2.length();
+    memo = new int[m][n];
+
+    int t = search(text1, text2, 0, 0);
+    System.out.println(Arrays.deepToString(memo));
+    return t;
+  }
+
+  private int search(String s1, String s2, int i, int j) {
+    if (i == s1.length() || j == s2.length()) {
+      return 0;
+    }
+    if (memo[i][j] > 0) {
+      return memo[i][j];
+    }
+    int result = 0;
+    if (s1.charAt(i) == s2.charAt(j)) {
+      result = 1 + search(s1, s2, i + 1, j + 1);
+    } else {
+      result = Math.max(search(s1, s2, i + 1, j), search(s1, s2, i, j + 1));
+    }
+    memo[i][j] = result;
+    return result;
   }
 }
+/**
+ * In this code, we first declare a LongestCommSubseq class with a main() method to run the program. Then, we declare a Solution class to solve the problem of finding the length of the longest common subsequence between two strings.
+ * The Solution class has a private memo variable to store the results of the subproblems. In the longestCommonSubsequence() method, we initialize the memo array with zeros and call the search() method to solve the problem.
+ * The search() method takes the two strings and two indices as input, and returns the length of the longest common subsequence between the substrings starting from the given indices.
+ * Inside the search() method, we first check if we have already computed the result for the current indices using the memo array. If we have, we return the stored result.
+ * Otherwise, we compute the result recursively by checking if the current characters of the two strings match or not. If they match, we add 1 to the result and move on to the next characters in both strings. If they do not match, we consider two options: either move to the next character in the first string or move to the next character in the second string, and take the maximum of the two.
+ * After computing the result, we store it in the memo array and return it.
+ * This implementation is more efficient than the original implementation because it uses memoization to avoid recomputing subproblems and updates the indices correctly in the recursive calls.
+ */
