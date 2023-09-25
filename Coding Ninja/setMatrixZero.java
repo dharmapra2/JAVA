ae57dccb06b06b5e5c8b5f523afa948a70af9753
Dharmapra2 <dharmapra865@gmail.com>
2023-09-25 22:00:52 +0530

    set matrix 0

--- Coding Ninja/setMatrixZero.java
+++ Coding Ninja/setMatrixZero.java
@@ -3,44 +3,64 @@ import java.util.Arrays;
 public class setMatrixZero {
 
   public static void main(String[] args) {
-    int[][] matrix = { { 1, 0, 2, 3 }, { 3, 4, 0, 2 }, { 1, 3, 1, 5 } };
-    int cornor = 1;
+    int[][] matrix = {
+      { 4, 3, 1, 4 },
+      { 9, 0, 1, 4 },
+      { 0, 2, 1, 3 },
+      { 70, 80, 1, 0 },
+    };
+
+    boolean fr = false, fc = false;
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        if (matrix[i][j] == 0) {
+          if (i == 0) fr = true;
+          if (j == 0) fc = true;
+          matrix[0][j] = 0;
+          matrix[i][0] = 0;
+        }
+      }
+    }
+    System.out.println();
+
     for (int[] a : matrix) {
       System.out.println(Arrays.toString(a));
     }
     System.out.println();
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[i].length; j++) {
-        if (matrix[i][j] == 0) {
-          if (i == 0) {
-            cornor = 0;
-            matrix[i][0] = 1;
-          } else {
-            matrix[i][0] = 0;
-            matrix[0][j] = 0;
-          }
-        }
-      }
-    }
-    for (int[] a : matrix) {
-      System.out.println(Arrays.toString(a));
-    }
-    System.out.println(cornor);
-
     for (int i = 1; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[i].length; j++) {
-        if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+      for (int j = 1; j < matrix[0].length; j++) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
           matrix[i][j] = 0;
         }
       }
     }
-    if (cornor == 0) {
-      for (int i = 0; i <= matrix.length; i++) {
-        matrix[0][i] = 0;
-      }
-    }
+    System.out.println();
+
     for (int[] a : matrix) {
       System.out.println(Arrays.toString(a));
     }
+    System.out.println();
+    if (fr) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        matrix[0][j] = 0;
+      }
+    }
+    System.out.println();
+
+    for (int[] a : matrix) {
+      System.out.println(Arrays.toString(a));
+    }
+    System.out.println();
+    if (fc) {
+      for (int i = 0; i < matrix.length; i++) {
+        matrix[i][0] = 0;
+      }
+    }
+    System.out.println();
+
+    for (int[] a : matrix) {
+      System.out.println(Arrays.toString(a));
+    }
+    System.out.println();
   }
 }
