92a43c4c21a02f8d8f71d874df5a41992e5a760a
Dharmapra2 <dharmapra865@gmail.com>
2024-02-21 22:31:51 +0530

    binarySearch0 of array.class of java

--- LeetCode Pratice/k_diff_pairs_in_an_array.java
+++ LeetCode Pratice/k_diff_pairs_in_an_array.java
@@ -46,4 +46,38 @@ class Innerk_diff_pairs_in_an_array {
     }
     return uniquePair.size();
   }
+
+  /**
+   * binarySearch0 is the internal method of java Array.class
+   * @param a
+   * @param fromIndex
+   * @param toIndex
+   * @param key
+   * @return
+   */
+  private static int binarySearch0(
+    int[] a,
+    int fromIndex,
+    int toIndex,
+    int key
+  ) {
+    int low = fromIndex;
+    int high = toIndex - 1;
+
+    while (low <= high) {
+      int mid = low + high >>> 1;
+      int midVal = a[mid];
+      if (midVal < key) {
+        low = mid + 1;
+      } else {
+        if (midVal <= key) {
+          return mid;
+        }
+
+        high = mid - 1;
+      }
+    }
+
+    return -(low + 1);
+  }
 }
